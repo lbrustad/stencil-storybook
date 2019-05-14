cee6057116909d87449fb9795c093c1c2020afa2
lbrustad <lasse.brustad@gmail.com>
2019-05-14 13:30:03 +0200

    objectified stories

--- src/components/vn-button/vn-button.stories.js
+++ src/components/vn-button/vn-button.stories.js
@@ -1,34 +1,21 @@
 import { storiesOf } from '@storybook/html';
 
 let btns = {
-  'Input 1': '<vn-button box-size="2">Test</vn-button>',
-  'Input 2': '<vn-button box-size="2">Test</vn-button>',
-  'Input 3': '<vn-button box-size="2">Test</vn-button>'
+    'Input 1': '<vn-button box-size="2">Mario</vn-button>',
+    'Input 2': '<vn-button box-size="2">Lasse</vn-button>',
+    'Input 3': '<vn-button box-size="2">Thor Arne</vn-button>'
 };
 
-storiesOf('Components/VnButton', module)
+let arr = [];
+for (let key in btns) {
+    arr.push(btns[key]);
+}
+
+const stories = storiesOf('Components/VnButton', module)
     .add(
         'Default',
-        () => `<vn-button></vn-button>`
-    )
-    .add(
-        'Input 1',
-        () => `<vn-button box-size="3">Lasse</vn-button>`
-              
-    )
-    .add(
-      'input 2',
-      () => `<vn-button box-size="3">Lasse</vn-button> 
-            <vn-button>Mario</vn-button> 
-            <vn-button>Thor</vn-button>`,
-    )
-    .add(
-      'Input 3',
-      () => `<vn-button box-size="3">Lasse</vn-button> 
-            <vn-button>Mario</vn-button> 
-            <vn-button>Thor</vn-button>`,
-  )
-    .add(
-        'Random Page',
-        () => `<vn-button value="Random greier"></vn-button>`
+        () => arr.join(' ')
     );
+for (let key in btns) {
+    stories.add(key, () => btns[key]);
+}
