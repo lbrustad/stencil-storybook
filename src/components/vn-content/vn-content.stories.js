18262e3d60b0c4ef562dd68f7627d649d7b0e874
lbrustad <lasse.brustad@gmail.com>
2019-05-23 15:40:55 +0200

    BOOOM - Changes

--- src/components/vn-content/vn-content.stories.js
+++ src/components/vn-content/vn-content.stories.js
@@ -1,9 +1,10 @@
 import { storiesOf } from '@storybook/html';
 
 storiesOf('Components/VnContent', module)
-  .add(
-    'example',
-    () => `<vn-content>
+    .add(
+        'example',
+        () => `<vn-content>
+      <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
       <h1>H1 / Apercu Light</h1>
       <h2>H2 / Apercu Regular</h2>
       <h3>H3 / Apercu Regular</h3>
@@ -11,4 +12,4 @@ storiesOf('Components/VnContent', module)
       <h5>H5 / Apercu Regular</h5>
       <h6>H6 / Apercu Medium</h6>
     </vn-content>`,
-  );
+    );
