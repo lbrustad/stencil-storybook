e52a5f4d6eb7c3f6eb42ffe5595f598c308c0c47
Thorabc <43777499+Thorabc@users.noreply.github.com>
2019-05-27 15:58:19 +0200

    Grid added and some changes.

--- src/components/my-build/my-build.stories.js
+++ src/components/my-build/my-build.stories.js
@@ -6,45 +6,34 @@ storiesOf('Components/MyBuild', module)
   <my-build>
     <vn-grid>
 
-      <vn-content>
-        <vn-header size="3">Test</vn-header>
+      <vn-content class="header">
+        <vn-header size="3">Dette er en overskrift</vn-header>
       </vn-content>
 
-      <vn-content></vn-content>
-      <vn-content></vn-content>
-
-      <vn-content>
-        <vn-content-title>
-          <vn-button color="p2" box-size="large" bold="true">
-            <vn-text>HJELP</vn-text>
-          </vn-button>
-        </vn-content-title>
+      <vn-content class="content">
+        <p class="body-en">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
       </vn-content>
 
-      <vn-content></vn-content>
-      <vn-content></vn-content>
-
-      <vn-content>
-        <vn-button>
-          <vn-content-label>
-            <vn-content-title>
-              <vn-header>
-                <vn-secondary-header>
-                  <vn-text>TEST MED ALLLLLLLT</vn-text>
-                </vn-secondary-header>
-              </vn-header>
-            </vn-content-title>
-          </vn-content-label>
+      <vn-content class="menu">
+        <vn-button color="p4" box-size="large">
+          <h4>start</h4>
+        </vn-button>
+        <br />
+        <vn-button color="p4" box-size="large">
+          <h4>start2</h4>
+        </vn-button>
+        <br />
+        <vn-button color="p4" box-size="large">
+          <h4>start3</h4>
+        </vn-button>
+        <br />
+        <vn-button color="p4" box-size="large">
+          <h4>start4</h4>
         </vn-button>
       </vn-content>
 
-      <vn-content></vn-content>
-      <vn-content></vn-content>
-      <vn-content></vn-content>
-      <vn-content></vn-content>
-      <vn-content></vn-content>
-      <vn-content>
-        <vn-header size="5">Mer testing på gang her selvfølgelig</vn-header>
+      <vn-content class="footer">
+        <vn-header size="5">Dette er en footer</vn-header>
       </vn-content>
     </vn-grid>
   </my-build>
