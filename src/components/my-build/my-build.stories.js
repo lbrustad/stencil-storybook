cd64e751204d0964846738af9fea0c157b2f6b42
lbrustad <lasse.brustad@gmail.com>
2019-05-28 14:10:35 +0200

    Added the amazing Stylusificion gridish systemificion

--- src/components/my-build/my-build.stories.js
+++ src/components/my-build/my-build.stories.js
@@ -6,15 +6,15 @@ storiesOf('Components/MyBuild', module)
   <my-build>
     <vn-grid>
 
-      <vn-content class="header">
+      <vn-content class="start-1 end-12-span">
         <vn-header size="3">Dette er en overskrift</vn-header>
       </vn-content>
 
-      <vn-content class="content">
+      <vn-content class="start-1 end-9-span">
         <p class="body-en">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
       </vn-content>
 
-      <vn-content class="menu">
+      <vn-content class="start-10 end-3-span">
         <vn-button color="p4" box-size="large">
           <h4>start</h4>
         </vn-button>
@@ -32,7 +32,7 @@ storiesOf('Components/MyBuild', module)
         </vn-button>
       </vn-content>
 
-      <vn-content class="footer">
+      <vn-content class="start-1 end-12-span">
         <vn-header size="5">Dette er en footer</vn-header>
       </vn-content>
     </vn-grid>
